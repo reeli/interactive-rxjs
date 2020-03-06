802d3de7eed20a91332ae7ae131421527100feb4
reeli <reeli.null@gmail.com>
2020-03-07 00:12:02 +0800

    feat: add basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { css, Global } from "@emotion/core";
-import { SubscribeDemo } from "src/examples/SubscribeDemo";
-import { CompleteDemo } from "src/examples/CompleteDemo";
-import { DemoWrapper } from "src/components/Demo";
-import { ErrorDemo } from "src/examples/ErrorDemo";
+import {css, Global} from "@emotion/core";
+import {SubscribeDemo} from "src/examples/SubscribeDemo";
+import {CompleteDemo} from "src/examples/CompleteDemo";
+import {DemoWrapper} from "src/components/Demo";
+import {ErrorDemo} from "src/examples/ErrorDemo";
+import {ConcatDemo} from "src/examples/ConcatDemo";
 
 export const App = () => {
-  return (
-    <div css={{ display: "flex", justifyContent: "center" }}>
-      <div css={{ padding: 25, width: 980 }}>
-        <Global
-          styles={css`
+    return (
+        <div
+            css={{
+                display: "flex",
+                position: "absolute",
+                left: 0,
+                top: 0,
+                bottom: 0,
+                right: 0,
+            }}
+        >
+            <aside css={{position: "fixed", left: 0, top: 0, bottom: 0, width: 200}}>
+                <a href={"#basic"}>基础部分</a>
+                <a href={"#operator"}>操作符</a>
+            </aside>
+            <main css={{padding: 25, flex: 1, marginLeft: 200}}>
+                <Global
+                    styles={css`
             html {
               font-size: 10px;
             }
@@ -19,34 +33,43 @@ export const App = () => {
               font-size: 1.4rem;
             }
           `}
-        />
-        <section>
-          <h1>基础部分</h1>
-          <div css={{ display: "flex", alignItems: "flex-end" }}>
-            <DemoWrapper>
-              <SubscribeDemo />
-            </DemoWrapper>
-            <DemoWrapper>
-              <CompleteDemo />
-            </DemoWrapper>
-            <DemoWrapper>
-              <ErrorDemo />
-            </DemoWrapper>
-          </div>
-        </section>
-        <section>
-          <h1>操作符</h1>
-          <DemoWrapper>
-            <div>合并数据流</div>
-          </DemoWrapper>
-        </section>
-        <section>
-          <h1>单播和多播</h1>
-        </section>
-        <section>
-          <h1>Hot Observable 与 Cold Observable</h1>
-        </section>
-      </div>
-    </div>
-  );
+                />
+                <section>
+                    <h1>
+                        <a id="basic" href={"#basic"}>
+                            基础部分
+                        </a>
+                    </h1>
+                    <div css={{display: "flex", alignItems: "flex-end"}}>
+                        <DemoWrapper>
+                            <SubscribeDemo/>
+                        </DemoWrapper>
+                        <DemoWrapper>
+                            <CompleteDemo/>
+                        </DemoWrapper>
+                        <DemoWrapper>
+                            <ErrorDemo/>
+                        </DemoWrapper>
+                    </div>
+                </section>
+                <section>
+                    <h1>
+                        <a id={"operator"} href={"#operator"}>
+                            操作符
+                        </a>
+                    </h1>
+                    <DemoWrapper>
+                        <div>合并数据流</div>
+                        <ConcatDemo/>
+                    </DemoWrapper>
+                </section>
+                <section>
+                    <h1>单播和多播</h1>
+                </section>
+                <section>
+                    <h1>Hot Observable 与 Cold Observable</h1>
+                </section>
+            </main>
+        </div>
+    );
 };
---
+++ src/examples/ConcatDemo.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { COLORS } from "src/style";
+import {DemoFooter, DemoHeader, DemoTitle} from "src/components/Demo";
+import { Button } from "src/components/Button";
+import { AnimatedCircles } from "src/components/AnimatedCircles";
+import { ObservableRect } from "src/components/ObservableRect";
+import { ObserverRect } from "src/components/ObserverRect";
+
+const data = ["1", "2"];
+
+export const ConcatDemo = () => {
+    const [completed, setCompleted] = useState(false);
+    const [started, setStarted] = useState(false);
+    const [reset, setReset] = useState(false);
+
+    return (
+        <div css={{ width: 200 }}>
+            <DemoTitle>Error</DemoTitle>
+            <DemoHeader>
+                <Button
+                    onClick={() => {
+                        setReset(false);
+                        setStarted(true);
+                    }}
+                    css={{ color: COLORS.BLUE }}
+                >
+                    开始动画
+                </Button>
+                <Button
+                    onClick={() => {
+                        setReset(true);
+                        setCompleted(false);
+                        setStarted(false);
+                    }}
+                    css={{ marginLeft: 5 }}
+                >
+                    重置动画
+                </Button>
+            </DemoHeader>
+            <svg width={"100%"} height={"100%"} viewBox={"0 0 200 300"}>
+                <line
+                    x1={100}
+                    x2={100}
+                    y1={50}
+                    y2={250}
+                    stroke={completed && !reset ? COLORS.ERROR : COLORS.GREEN}
+                    strokeWidth={2}
+                />
+                {reset ? null : (
+                    <AnimatedCircles
+                        data={data}
+                        completed={completed}
+                        started={started}
+                        onReset={() => {
+                            setCompleted(true);
+                        }}
+                    />
+                )}
+                <ObservableRect />
+                <ObserverRect />
+            </svg>
+            <DemoFooter>{completed && !reset && <div>出错了！</div>}</DemoFooter>
+        </div>
+    );
+};
