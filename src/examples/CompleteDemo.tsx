f55cfa1032ebc927fdb110a445b319ac3d013ae4
reeli <reeli.null@gmail.com>
2020-03-06 20:25:32 +0800

    add reset function for CompleteDemo.tsx

--- src/examples/CompleteDemo.tsx
+++ src/examples/CompleteDemo.tsx
@@ -5,37 +5,98 @@ import { COLORS } from "src/style";
 
 const data = ["1", "2", "3"];
 
+const calcFromY = (completed: boolean, itemY: number) => {
+  if (completed) {
+    return itemY;
+  }
+  return 16;
+};
+
+const calcToY = (started: boolean, itemY: number) => {
+  if (started) {
+    return itemY;
+  }
+  return 16;
+};
+
+const CircleSequence: React.FC<{
+  completed: boolean;
+  started: boolean;
+  onReset?: () => void;
+}> = ({ completed, started, onReset }) => (
+  <>
+    {map(data, (text, i) => {
+      const itemY = 220 - i * 30;
+      return (
+        <Spring
+          from={{ y: calcFromY(completed, itemY) }}
+          to={{
+            y: calcToY(started, itemY),
+          }}
+          delay={i * 1000}
+          key={i}
+          onRest={() => {
+            if (i === data.length - 1) {
+              onReset && onReset();
+            }
+          }}
+        >
+          {styles => (
+            <g transform={`translate(100, ${styles.y})`}>
+              <circle cx="0" cy="0" r="15" fill={COLORS.WHITE} strokeWidth={2} stroke={COLORS.BLACK} />
+              <text x={0} y={6} css={{ fontSize: "2rem" }} textAnchor={"middle"}>
+                {text}
+              </text>
+            </g>
+          )}
+        </Spring>
+      );
+    })}
+  </>
+);
+
 export const CompleteDemo = () => {
-  const [resetNumber, setResetNumber] = useState(0);
+  const [completed, setCompleted] = useState(false);
+  const [started, setStarted] = useState(false);
+  const [reset, setReset] = useState(false);
 
   return (
     <div css={{ width: 200 }}>
-      <div onClick={() => setResetNumber(resetNumber + 1)}>Start Animation</div>
+      <button
+        onClick={() => {
+          setReset(false);
+          setStarted(true);
+        }}
+      >
+        开始动画
+      </button>
+      <button
+        onClick={() => {
+          setReset(true);
+          setCompleted(false);
+          setStarted(false);
+        }}
+      >
+        重置动画
+      </button>
       <svg width={"100%"} height={"100%"} viewBox={"0 0 200 300"}>
-        <line x1={100} x2={100} y1={50} y2={250} stroke={COLORS.BLACK} strokeWidth={2} />
-        {map(data, (text, i) => (
-          <Spring
-            from={{ y: 16 }}
-            to={{
-              y: resetNumber ? 220 - i * 30 : 16,
+        <line
+          x1={100}
+          x2={100}
+          y1={50}
+          y2={250}
+          stroke={completed && !reset ? COLORS.GREY : COLORS.BLACK}
+          strokeWidth={2}
+        />
+        {reset ? null : (
+          <CircleSequence
+            completed={completed}
+            started={started}
+            onReset={() => {
+              setCompleted(true);
             }}
-            delay={i * 1500}
-            reset={!!resetNumber}
-            key={i}
-            onRest={()=>{
-              console.log("test")
-            }}
-          >
-            {styles => (
-              <g transform={`translate(100, ${styles.y})`}>
-                <circle cx="0" cy="0" r="15" fill={COLORS.WHITE} strokeWidth={2} stroke={COLORS.BLACK} />
-                <text x={0} y={6} css={{ fontSize: "2rem" }} textAnchor={"middle"}>
-                  {text}
-                </text>
-              </g>
-            )}
-          </Spring>
-        ))}
+          />
+        )}
         <g>
           <rect x={0} y={0} width={200} height={50} fill={COLORS.OBSERVABLE} />
           <text x={100} y={25} css={{ fontSize: "1.4rem" }} textAnchor={"middle"}>
@@ -49,6 +110,9 @@ export const CompleteDemo = () => {
           </text>
         </g>
       </svg>
+      {completed && !reset && (
+        <div>Complete, Observable 已完结，表示「没有更多数据了」，之后也不会再向 Observer 推送数据</div>
+      )}
     </div>
   );
 };
