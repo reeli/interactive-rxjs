92b5cbbdf780d3b710ae42153fc8fb435ef71de7
reeli <reeli.null@gmail.com>
2020-03-07 22:37:17 +0800

    feat: support text function in Circle.tsx

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,40 +1,44 @@
-import { COLORS } from "src/style";
+import {COLORS} from "src/style";
 import React from "react";
-import { animated } from "react-spring";
+import {animated} from "react-spring";
+import {isFunction} from "lodash";
 
 const CIRCLE_CONFIG = {
-  radius: 15,
-  stroke: COLORS.BLACK,
-  strokeWidth: 2,
-  animateY: 220,
-  fill: COLORS.WHITE,
+    radius: 15,
+    stroke: COLORS.BLACK,
+    strokeWidth: 2,
+    animateY: 220,
+    fill: COLORS.WHITE,
 };
 
+type TTextFn = () => string | number;
+
 interface ICircleProps {
-  text?: string | number;
-  r?: number;
-  fill?: string;
-  strokeWidth?: number;
-  translateX?: number;
-  translateY?: number;
-  stroke?: string;
+    text?: TTextFn | string | number;
+    r?: number;
+    fill?: string;
+    strokeWidth?: number;
+    translateX?: number;
+    translateY?: number;
+    stroke?: string;
 }
 
 export const Circle: React.FC<ICircleProps> = animated(
-  ({
-    r = CIRCLE_CONFIG.radius,
-    fill = CIRCLE_CONFIG.fill,
-    strokeWidth = CIRCLE_CONFIG.strokeWidth,
-    text,
-    translateX = 100,
-    translateY = 0,
-    stroke = CIRCLE_CONFIG.stroke,
-  }) => (
-    <g transform={`translate(${translateX}, ${translateY})`}>
-      <circle cx="0" cy="0" r={r} fill={fill} strokeWidth={strokeWidth} stroke={stroke} />
-      <text x={0} y={CIRCLE_CONFIG.radius / 2} textAnchor={"middle"} css={{ fontSize: "2rem" }}>
-        {text}
-      </text>
-    </g>
-  ),
+    ({
+         r = CIRCLE_CONFIG.radius,
+         fill = CIRCLE_CONFIG.fill,
+         strokeWidth = CIRCLE_CONFIG.strokeWidth,
+         text,
+         translateX = 100,
+         translateY = 0,
+         stroke = CIRCLE_CONFIG.stroke,
+         ...others
+     }) => (
+        <g transform={`translate(${translateX}, ${translateY})`} {...others}>
+            <circle cx="0" cy="0" r={r} fill={fill} strokeWidth={strokeWidth} stroke={stroke}/>
+            <text x={0} y={CIRCLE_CONFIG.radius / 2} textAnchor={"middle"} css={{fontSize: "2rem"}}>
+                {isFunction(text) ? text() : text}
+            </text>
+        </g>
+    ),
 );
