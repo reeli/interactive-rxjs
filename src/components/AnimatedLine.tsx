f6d51cf0618f426e86c672f91acbf6b1606aeb75
reeli <reeli.null@gmail.com>
2020-03-07 11:50:01 +0800

    feat: correct props in AnimatedLine

--- src/components/AnimatedLine.tsx
+++ src/components/AnimatedLine.tsx
@@ -1,6 +1,6 @@
 import { animated } from "react-spring";
 import React from "react";
 
-export const AnimatedLine = animated(({ style, x1 = 100, y1 = 100, strokeWidth = 2, ...otherProps }) => (
-  <line x1={x1} x2={y1} strokeWidth={strokeWidth} {...otherProps} {...style} />
+export const AnimatedLine = animated(({ style, x1 = 100, x2 = 100, y1 = 100, strokeWidth = 2, ...otherProps }) => (
+  <line x1={x1} x2={x2} strokeWidth={strokeWidth} y1={y1} {...otherProps} {...style} />
 ));
