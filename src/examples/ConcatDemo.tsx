6b97e68f3328f26a4af212eb2d21a4c48df9a307
reeli <reeli.null@gmail.com>
2020-03-07 14:07:40 +0800

    feat: setup concat time sequences

--- src/examples/ConcatDemo.tsx
+++ src/examples/ConcatDemo.tsx
@@ -1,11 +1,11 @@
-import React, { useRef, useState } from "react";
-import { COLORS } from "src/style";
-import { DemoFooter, DemoHeader, DemoTitle } from "src/components/Demo";
-import { Button } from "src/components/Button";
-import { Rect } from "src/components/Rect";
-import { ObserverRect } from "src/components/ObserverRect";
-import { AnimatedLine, useAnimatedLine } from "src/components/AnimatedLine";
-import { useChain } from "react-spring";
+import React, {useRef, useState} from "react";
+import {COLORS} from "src/style";
+import {DemoFooter, DemoHeader, DemoTitle} from "src/components/Demo";
+import {Button} from "src/components/Button";
+import {Rect} from "src/components/Rect";
+import {ObserverRect} from "src/components/ObserverRect";
+import {AnimatedLine, useAnimatedLine} from "src/components/AnimatedLine";
+import {useChain} from "react-spring";
 
 const LINE_CONFIG = {
   OBSERVER_TO_CONCAT: {
@@ -30,6 +30,8 @@ const LINE_CONFIG = {
 
 export const ConcatDemo = () => {
   const [started, setStarted] = useState<boolean | null>(null);
+  const [isSource1RefComplete, setIsSource1RefComplete] = useState<boolean | null>(null);
+  const [isSource2RefComplete, setIsSource2RefComplete] = useState<boolean | null>(null);
 
   const concatRef = useRef<any>();
   const styleConcat = useAnimatedLine({
@@ -45,9 +47,24 @@ export const ConcatDemo = () => {
     y2: LINE_CONFIG.CONCAT_TO_SOURCE1.y2,
     started,
     elementRef: source1Ref,
+    onReset: () => {
+      setIsSource1RefComplete(true);
+    },
   });
 
-  useChain(started ? [concatRef, source1Ref] : [source1Ref, concatRef], [0, 0.4]);
+  const source2Ref = useRef<any>();
+  const styleSource2 = useAnimatedLine({
+    y1: LINE_CONFIG.CONCAT_TO_SOURCE2.y1,
+    y2: LINE_CONFIG.CONCAT_TO_SOURCE2.y2,
+    started: isSource1RefComplete,
+    elementRef: source2Ref,
+    onReset: () => {
+      setIsSource2RefComplete(true);
+    },
+  });
+
+  useChain(started ? [concatRef, source1Ref] : [source1Ref, concatRef], [0, 0.5, 1.2]);
+  useChain([source2Ref], [0.5]);
 
   return (
     <div css={{ width: 200 }}>
@@ -64,6 +81,8 @@ export const ConcatDemo = () => {
         <Button
           onClick={() => {
             setStarted(null);
+            setIsSource1RefComplete(null);
+            setIsSource2RefComplete(null);
           }}
           css={{ marginLeft: 5 }}
         >
@@ -71,9 +90,21 @@ export const ConcatDemo = () => {
         </Button>
       </DemoHeader>
       <svg width={"100%"} height={"100%"} viewBox={"0 0 200 300"}>
-        <AnimatedLine {...LINE_CONFIG.OBSERVER_TO_CONCAT} stroke={COLORS.GREEN} style={styleConcat} />
-        <AnimatedLine {...LINE_CONFIG.CONCAT_TO_SOURCE1} stroke={COLORS.GREEN} style={styleSource1} />
-        <AnimatedLine {...LINE_CONFIG.CONCAT_TO_SOURCE2} stroke={COLORS.GREEN} />
+        <AnimatedLine
+          {...LINE_CONFIG.OBSERVER_TO_CONCAT}
+          stroke={isSource2RefComplete ? COLORS.GREY : COLORS.GREEN}
+          style={styleConcat}
+        />
+        <AnimatedLine
+          {...LINE_CONFIG.CONCAT_TO_SOURCE1}
+          stroke={isSource1RefComplete ? COLORS.GREY : COLORS.GREEN}
+          style={styleSource1}
+        />
+        <AnimatedLine
+          {...LINE_CONFIG.CONCAT_TO_SOURCE2}
+          stroke={isSource2RefComplete ? COLORS.GREY : COLORS.GREEN}
+          style={styleSource2}
+        />
         <Rect width={80} height={40} text={"Source1$"} />
         <Rect width={80} height={40} x={120} text={"Source2$"} />
         <Rect width={200} height={40} y={120} text={"Concat$"} />
