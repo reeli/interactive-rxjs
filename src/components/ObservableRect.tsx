326e5981dc4604d881e887b814baf1924c4993b9
reeli <reeli.null@gmail.com>
2020-03-07 11:02:08 +0800

    refactor: use Rect component in ObservableRect and ObserverRect

--- src/components/ObservableRect.tsx
+++ src/components/ObservableRect.tsx
@@ -1,11 +1,5 @@
 import { COLORS } from "src/style";
 import React from "react";
+import {Rect} from "src/components/Rect";
 
-export const ObservableRect = () => (
-  <g>
-    <rect x={0} y={0} width={200} height={50} fill={COLORS.OBSERVABLE} />
-    <text x={100} y={25} css={{ fontSize: "1.4rem" }} textAnchor={"middle"}>
-      可被观察的对象 Observable
-    </text>
-  </g>
-);
+export const ObservableRect: React.FC = () => <Rect fill={COLORS.OBSERVABLE} text={"可被观察的对象 Observable"} />;
--- src/components/ObserverRect.tsx
+++ src/components/ObserverRect.tsx
@@ -1,12 +1,9 @@
 import { COLORS } from "src/style";
 import React from "react";
+import {Rect} from "src/components/Rect";
 
 export const ObserverRect: React.FC = ({ children }) => (
-  <g>
-    <rect x={0} y={250} width={200} height={50} fill={COLORS.OBSERVER} />
-    <text x={100} y={275} css={{ fontSize: "1.4rem" }} textAnchor={"middle"}>
-      观察者 Observer
-    </text>
+  <Rect fill={COLORS.OBSERVER} y={250} text={"观察者 Observer"}>
     {children}
-  </g>
+  </Rect>
 );
