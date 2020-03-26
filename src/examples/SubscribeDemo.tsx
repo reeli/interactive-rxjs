69fc7b08651271fdb02846d7ea179aac2e0fcaff
reeli <reeli.null@gmail.com>
2020-03-26 18:38:48 +0800

    feat: extract useBasicLineAnimation.ts from SubscribeDemo.tsx

--- src/examples/SubscribeDemo.tsx
+++ src/examples/SubscribeDemo.tsx
@@ -1,4 +1,3 @@
-import { useSpring } from "react-spring";
 import React, { useState } from "react";
 import { COLORS } from "src/style";
 import { DemoFooter, DemoHeader, DemoTitle, DemoWrapper } from "src/components/Demo";
@@ -7,24 +6,11 @@ import { isNull } from "lodash";
 import { AnimatedLine } from "src/components/AnimatedLine";
 import { ObservableRect } from "src/components/ObservableRect";
 import { ObserverRect } from "src/components/ObserverRect";
-
-const LINE_CONFIG = {
-  y1: 250,
-  y2: 50,
-};
+import {useBasicLineAnimation} from "src/hooks/useBasicLineAnimation";
 
 export const SubscribeDemo = () => {
-  const [toggle, setToggle] = useState<boolean | null>(null);
-  const props = useSpring({
-    from: {
-      y1: LINE_CONFIG.y1,
-      y2: toggle ? LINE_CONFIG.y2 : LINE_CONFIG.y1,
-    },
-    to: {
-      y1: LINE_CONFIG.y1,
-      y2: toggle ? LINE_CONFIG.y2 : LINE_CONFIG.y1,
-    },
-  });
+  const [subscribed, setSubscribed] = useState<boolean | null>(false);
+  const lineAnimationStyle = useBasicLineAnimation(subscribed);
 
   return (
     <DemoWrapper>
@@ -32,22 +18,22 @@ export const SubscribeDemo = () => {
       <DemoHeader>
         <Button
           onClick={() => {
-            setToggle(!toggle);
+            setSubscribed(!subscribed);
           }}
           css={{ color: COLORS.BLUE }}
         >
-          {toggle ? "退订 unsubscribe" : "订阅 subscribe"}
+          {subscribed ? "退订 unsubscribe" : "订阅 subscribe"}
         </Button>
       </DemoHeader>
       <div css={{ width: 200 }}>
         <svg width={"100%"} height={"100%"} viewBox={"0 0 200 300"}>
-          <AnimatedLine style={props} stroke={COLORS.GREEN} />
+          <AnimatedLine style={lineAnimationStyle} stroke={COLORS.GREEN} />
           <ObservableRect />
           <ObserverRect />
         </svg>
       </div>
       <DemoFooter>
-        {isNull(toggle) ? null : toggle ? <div>已订阅，现在可以向观察者推送数据了</div> : <div>已取消订阅</div>}
+        {isNull(subscribed) ? null : subscribed ? <div>已订阅，现在可以向观察者推送数据了</div> : <div>已取消订阅</div>}
       </DemoFooter>
     </DemoWrapper>
   );
---
+++ src/hooks/useBasicLineAnimation.ts
@@ -0,0 +1,28 @@
+import { useSpring } from "react-spring";
+
+const LINE_CONFIG = {
+  y1: 250,
+  y2: 50,
+};
+
+export const useBasicLineAnimation = (
+  toggle: boolean | null = null,
+  onAnimationDone?: () => any,
+  y1 = LINE_CONFIG.y1,
+  y2 = LINE_CONFIG.y2,
+) =>
+  useSpring({
+    from: {
+      y1,
+      y2: toggle ? y2 : y1,
+    },
+    to: {
+      y1: y1,
+      y2: toggle ? y2 : y1,
+    },
+    onRest: () => {
+      if (toggle) {
+        onAnimationDone && onAnimationDone();
+      }
+    },
+  });
