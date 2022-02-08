45277441bd9ad8bf9fd249b095445c1d5f3d3d0a
reeli <reeli.null@gmail.com>
2022-02-08 18:20:01 +0800

    feat: add missing packages

--- scripts/index.ts
+++ scripts/index.ts
@@ -6,6 +6,7 @@ const HTML_PATH = path.resolve(process.cwd(), "public/index.html");
 const fileStr = fs.readFileSync(HTML_PATH, "utf-8");
 const $ = cheerio.load(fileStr);
 
+// @ts-ignore
 $("head").prepend("<base href='https://reeli.github.io/interactive-rxjs/'/>");
 
 fs.writeFileSync(HTML_PATH, $.html());
